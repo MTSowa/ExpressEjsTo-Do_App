6bb9dbc07dcd9fd9e1d1f9f72046aae076721fcb
MTSowa <sowaf57@gmail.com>
2021-09-08 12:39:45 +0000

    Add 2nd page, use include, ejs variables

--- todo-list/server.js
+++ todo-list/server.js
@@ -8,28 +8,51 @@ const app = express();
 app.set('view engine', 'ejs');
 // use body-parser
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 // app.use(express.json());
 
-// %%%%%%%%%%%%%%%%%%%%%%%% VARIABLES %%%%%%%%%%%%%%%%%%%%%%%%%%%
-var inputData = [];
-const todoItems = ['one', 'two', 'three'];
+// %%%%%%%%%%%%%%%%%%%%%%%%   VARIABLES   %%%%%%%%%%%%%%%%%%%%%%%%%%%
+let inputData = ['one', 'two', 'three'];
+let todoItems = ['document stred', 'three contracts'];
 
 // %%%%%%%%%%%%%%%%%%%%%%%% GET  REQUESTS %%%%%%%%%%%%%%%%%%%%%%%%%%%
 app.get('/', (req, res) => {
   res.render('list', {
-    name: 'mt@sowa',
-    btn: 'Clickable button',
+    page: 'home',
     liItem: inputData,
   });
 });
+app.get('/work', (req, res) => {
+  res.render('list', {
+    page: 'work',
+    liItem: todoItems,
+  });
+});
+app.get('/about',(req,res)=>{
+  res.render('about');
+})
 
 // %%%%%%%%%%%%%%%%%%%%%%%% POST REQUESTS %%%%%%%%%%%%%%%%%%%%%%%%%%%
 app.post('/', (req, res) => {
-  const inpData = req.body.todo;
-  inputData = [...inputData, inpData];
+  console.log(req.body);
+  if (req.body.list === 'work') {
+    const inpDat = req.body.todo;
+    todoItems = [...todoItems, inpDat];
 
-  res.redirect('/');
+    res.redirect('/work');
+  } else {
+    const inpData = req.body.todo;
+    inputData = [...inputData, inpData];
+
+    res.redirect('/');
+  }
 });
+// app.post('/work', (req, res) => {
+//   const inpDat = req.body.todo;
+//   todoItems = [...todoItems, inpDat];
+
+//   res.redirect('/work');
+// });
 
 app.listen(4000, function () {
   console.log('the port is functional...');
